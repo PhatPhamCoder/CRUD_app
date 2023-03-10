b79578cd0063a9bd759b9d31396b822903969957
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-03-10 18:54:08 +0700

    video#7

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,15 @@ import Head from "next/head";
 import { BiUserPlus } from "react-icons/bi";
 import Form from "@/components/form";
 import Table from "@/components/table";
+import { useState } from "react";
 
 export default function Home() {
+  const [visible, setVisible] = useState(false);
+
+  const handler = () => {
+    setVisible(visible ? false : true);
+  };
+
   return (
     <section>
       <Head>
@@ -17,7 +24,10 @@ export default function Home() {
         </h1>
         <div className="container mx-auto flex justify-between py-5 border-b">
           <div className="flex left gap-3">
-            <button className="flex items-center gap-2 bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:text-indigo-500 hover:text-gray-800">
+            <button
+              onClick={handler}
+              className="flex items-center gap-2 bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:text-indigo-500 hover:text-gray-800"
+            >
               Add Employee
               <BiUserPlus size={32} />
             </button>
@@ -25,7 +35,7 @@ export default function Home() {
         </div>
         {/* Collapsable form */}
         <div className="container mx-auto py-5">
-          <Form></Form>
+          {visible ? <Form></Form> : <></>}
         </div>
         {/* Table */}
         <div className="container mx-auto">
