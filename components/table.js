25a73e3fc40dcb157df7d62976a87ffd00d5e5fb
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-03-17 11:08:31 +0700

    video#18

--- components/table.js
+++ components/table.js
@@ -1,11 +1,15 @@
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
-import data from "../database/data.json";
+import { getUsers } from "@/lib/helper";
+import { useQuery } from "react-query";
 
 export default function Table() {
+  const { isLoading, isError, data, error } = useQuery("users", getUsers);
+  if (isLoading) return <div>Emloyment is loading....</div>;
+  if (isError) return <div>Got error{error}</div>;
   return (
     <table className="min-w-full table-auto">
       <thead>
-        <tr className="bg-gray-800">
+        <tr className="bg-gray-800 rounded-xl">
           <th className="px-16 py-2">
             <span className="text-gray-200">Name</span>
           </th>
@@ -27,19 +31,23 @@ export default function Table() {
         </tr>
       </thead>
       <tbody className="bg-gray-200">
-        {data.map((obj, index) => (
-          <Tr {...obj} key={index} />
+        {data.map((value, index) => (
+          <Tr {...value} key={index} />
         ))}
       </tbody>
     </table>
   );
 }
 
-function Tr({ name, avatar, email, salary, date, status }) {
+function Tr({ id, name, avatar, email, salary, date, status }) {
   return (
     <tr className="bg-gray-50 text-center">
-      <td className="px-16 py-3 flex flex-row items-center">
-        <img src={avatar || "#"} alt="" />
+      <td className="px-16 py-2 flex flex-row items-center">
+        <img
+          src={avatar || "#"}
+          alt=""
+          className="h-8 w-8 rounded-full onject-cover"
+        />
         <span className="text-center ml-2 font-semibold">
           {name || "Unknow"}
         </span>
@@ -55,7 +63,11 @@ function Tr({ name, avatar, email, salary, date, status }) {
       </td>
       <td className="px-16 py-2">
         <button className="cursor">
-          <span className="bg-green-500 text-white p-2 rounded-2xl">
+          <span
+            className={`${
+              status == "Active" ? "bg-green-500" : "bg-rose-500"
+            } text-white p-2 rounded-2xl`}
+          >
             {status || "Unknow"}
           </span>
         </button>
